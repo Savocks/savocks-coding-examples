814747bedf62039c85b35106c81f5f98e25b8da5
Savocks <giulio.savoca97@gmail.com>
2024-07-19 15:39:40 +0200

    Update webpack production configuration
    Replaced the CopyPlugin with MiniCssExtractPlugin in the webpack config for production. This update includes changes in module rules for Javascript and CSS files and sets up devServer for liveReload and hot-reload for better development experience. The HtmlWebpackPlugin

--- web-components-part-2/webpack.config.prod.js
+++ web-components-part-2/webpack.config.prod.js
@@ -1,26 +1,46 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
 
 module.exports = merge(common, {
   mode: 'production',
+  devtool: 'inline-source-map',
+  devServer: {
+    liveReload: true,
+    hot: true,
+    open: true,
+    static: ['./'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        // Exclude CSS files in 'components' directory from this rule
+        exclude: path.resolve(__dirname, 'src/components'),
+      },
+      {
+        test: /\.css$/,
+        use: ['raw-loader'],
+        // Only apply this rule to CSS files in 'components' directory
+        include: path.resolve(__dirname, 'src/components'),
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: 'public/index.html',
     }),
-    new CopyPlugin({
-      patterns: [
-        { from: 'img', to: 'img' },
-        { from: 'css', to: 'css' },
-        { from: 'js/vendor', to: 'js/vendor' },
-        { from: 'icon.svg', to: 'icon.svg' },
-        { from: 'favicon.ico', to: 'favicon.ico' },
-        { from: 'robots.txt', to: 'robots.txt' },
-        { from: 'icon.png', to: 'icon.png' },
-        { from: '404.html', to: '404.html' },
-        { from: 'site.webmanifest', to: 'site.webmanifest' },
-      ],
-    }),
-  ],
+    new MiniCssExtractPlugin()
+  ]
 });
